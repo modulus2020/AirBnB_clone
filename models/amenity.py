1b05577fce55781aa11436a6c1fd7c695f10221d
Geneovo <bodjuvwederhie@gmail.com>
2024-02-11 08:23:51 -0800

    Amenity file

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines the Amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class for storing information about amenities"""
+
+    name = ""
